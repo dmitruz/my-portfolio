146dcccf623453fbf5124f7e4862b2e1f77c3b16
dmitruz <dmitruz970@gmail.com>
2023-07-08 16:23:16 +0300

    ee

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburger');
-const navList = document.getElementById('nav-list');
-
-const toggleButton = () => {
-  navList.classList.toggle('show');
-};
-
-hamburgerBtn.addEventListener('click', toggleButton);
