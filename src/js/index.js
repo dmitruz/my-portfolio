fa961279a795d46b09327e5c6b0ac73362ad635a
dmitruz <dmitruz970@gmail.com>
2024-01-24 22:33:38 +0200

    replace picture

--- src/js/index.js
+++ src/js/index.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-  strings: ['Frontend Developer', 'Web Designer'],
+  strings: ['Frontend Developer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
