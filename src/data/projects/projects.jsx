05286ae0d875f1e65026453c1c31e9a8030bdb78
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-04-23 14:06:23 +0300

    add project section

---
+++ src/data/projects/projects.jsx
@@ -0,0 +1,28 @@
+import project1 from "../images/project1.png";
+import project2 from "../images/project2.png";
+import project3 from "../images/project3.png";
+
+
+export const projects = [
+    {
+        title: "Search Movie Website.",
+        description: "A dynamic web application designed to help cinephiles discover their next favorite film through advanced filtering and real-time data fetching.",
+        image: project1,
+        link: "https://korolovdmitriy.github.io/filmoteka-team-project/",
+        code: "https://github.com/dmitruz/filmoteka-team-project"
+    },
+    {
+        title: "Personal Calorie Tracker",
+        description: "A health-focused utility app that calculates a user’s Total Daily Energy Expenditure (TDEE) to provide personalized weight loss goals.",
+        image: project2,
+        link: "https://weightloss-webhive.netlify.app/",
+        code: "https://github.com/dmitruz/lose-weight-app"
+    },
+    {
+        title: "SavorySeeker | Culinary Discovery Engine",
+        description: "A vibrant recipe application designed to bridge the gap between what's for dinner? and a finished meal. Users can explore random inspirations or perform targeted searches for specific ingredients or cuisines.",
+        image: project3,
+        link: "https://restaurant-dishes.netlify.app/",
+        code: "https://github.com/dmitruz/restaurant-menu"
+    },
+];
